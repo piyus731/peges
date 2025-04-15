0f4cabc3df9e0f769bb8e57b697c749e0226b70c
piyus731 <piyushkarn014@gmail.com>
2025-04-16 01:30:54 +0530

    change of function of the code and write now their are problrm of the code

---
+++ app.js
@@ -0,0 +1,70 @@
+let h1=document.querySelector("h1");
+let gameSeq=[];
+let userSeq=[];
+
+let btns=["yellow","red","purple","green"];
+
+let started=false;
+let level=0;
+
+let h2=document.querySelector("h2");
+
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("game is started");
+    started=true;
+    levelUp();
+    }
+});
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+
+    },250);
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+
+    },250);
+}
+
+
+function levelUp(){
+    level++;
+    h2.innerText=`level ${level}`;
+    let randidx=Math.floor(Math.random()*3);
+    // let randColor=btns[randidx];
+    // let randbtn=document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+
+    gameFlash(randbtn);
+
+}
+
+function checkAns(){
+    console.log("curr level",level);
+
+}
+
+function btnPress(){
+    let btn=this;
+
+   userFlash(btn);
+
+   usercolor=btn.getAttribute("id");
+   userSeq.push(userColor);
+
+
+   checkAns();
+}
+let allbtns=document.querySelectorAll(".btn");
+for(btn of allbtns){
+    btn.addEventListener("click",btnPress);
+}
+
